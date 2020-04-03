2d61a9c78136c28e7397d33b7640fe0756e905ce
Nhat-Tien Nguyen <nhattn38@tamu.edu>
2020-04-03 21:46:46 +0000

    Created and added the ruby script

---
+++ ruby/park.rb
@@ -0,0 +1,9 @@
+print "Please enter your favorite food: "
+food = gets.chomp
+print "Please enter a person's name: "
+noun = gets.chomp
+print "Please enter a person's name again: "
+prop_noun = gets.chomp
+
+
+print "#{prop_noun} and #{noun} got \n#{food} and ate at the park.\n"
