a8885d60c3d96523d8dd207dd9df855d96728bc6
Nhat-Tien Nguyen <nhattn38@tamu.edu>
2020-04-03 21:45:09 +0000

    Created and added perl script

---
+++ perl/currentdate.pl
@@ -0,0 +1,6 @@
+@months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+@days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
+
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+print "$mday $months[$mon] $days[$wday]\n";
+
